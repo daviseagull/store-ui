11be00a17645d326772a4c87338720b78107b357
An-225 <66622436+An-225@users.noreply.github.com>
2023-03-23 22:17:25 -0300

    feat: values calculation working

---
+++ checkout/checkout.js
@@ -0,0 +1,49 @@
+const discount = 10.00
+
+let items = [
+    {
+        "unitPrice": 110.00,
+        "currentPrice": 220.00,
+        "quantity": 2
+    },
+    {
+        "unitPrice": 55.00,
+        "currentPrice": 110.00,
+        "quantity": 2
+    }
+]
+
+function calculateValues(itemIndex, operation) {
+    let item = items[itemIndex]
+    let itemTotalElement = document.getElementById(`product_${itemIndex}_total`)
+    let itemQuantityElement = document.getElementById(`product_${itemIndex}_quantity`)
+    let subTotalElement = document.getElementById(`sub_total`)
+    let grandTotalElement = document.getElementById(`grand_total`)
+
+    switch (operation) {
+        case '+':
+            item.quantity++;
+            item.currentPrice += item.unitPrice
+            break;
+
+        case '-':
+            if (item.quantity < 1)
+                break;
+            item.quantity--;
+            item.currentPrice -= item.unitPrice
+
+            break;
+
+        default:
+            console.log("Tá maluco?????");
+            break;
+    }
+
+    let subTotal = items.reduce((previus, current) => { return previus + current.currentPrice }, 0)
+    let total = subTotal === 0 ? 0 : subTotal - discount
+
+    itemTotalElement.textContent = `R$ ${item.currentPrice.toFixed(2).replace('.', ',')}`
+    itemQuantityElement.textContent = item.quantity
+    subTotalElement.textContent = `R$ ${total.toFixed(2).replace('.', ',')}`
+    grandTotalElement.textContent = `R$ ${(subTotal).toFixed(2).replace('.', ',')}`
+}
