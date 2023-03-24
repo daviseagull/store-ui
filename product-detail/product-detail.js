3a9d753690c8f13d31982003bf23c082d1e9ff8e
An-225 <66622436+An-225@users.noreply.github.com>
2023-03-23 21:11:34 -0300

    feat: image preview working

--- product-detail/product-detail.js
+++ product-detail/product-detail.js
@@ -1,5 +1,7 @@
 function ChangeImage(event) {
-	const nextPreview = event.target;
-
-	document.getElementById('previewer').setAttribute('background-image', nextPreview.getAttribute('background-image'));
+	console.log("Entrei");
+	const nextPreviewStyle = getComputedStyle(event.target);
+	let currentPreviewStyle = getComputedStyle(document.getElementById('previewer'));
+	document.getElementById('previewer').style.backgroundImage = nextPreviewStyle.backgroundImage;
+	// currentPreviewStyle.backgroundImage = nextPreviewStyle.backgroundImage
 }
